2b3d7dae1d4f654a14f3e5f35441677cfc676f5b
kishima <kishima7@gmail.com>
2018-06-21 00:34:05 +0900

    add a LED example

---
+++ examples/control_LED/control_LED.ino
@@ -0,0 +1,22 @@
+#include <libmrubyc.h>
+
+#define MEMSIZE (1024*30)
+static uint8_t mempool[MEMSIZE];
+
+void setup() {
+  delay(1000);
+
+  Serial.println("--- begin setup"); 
+  mrbc_init(mempool, MEMSIZE);
+  mrbc_define_methods();
+  if(NULL == mrbc_create_task( code, 0 )){
+    Serial.println("mrbc_create_task error");
+    return;
+  }
+  Serial.println("--- run mruby script"); 
+  mrbc_run();
+}
+
+void loop() {
+  delay(1000);
+}
