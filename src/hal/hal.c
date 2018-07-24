6885ed2a0e164c9775bb3d518d32e750a5b06059
kishima <kishima7@gmail.com>
2018-07-25 01:55:17 +0900

    add null character for the string in hal_write

--- src/hal/hal.c
+++ src/hal/hal.c
@@ -12,15 +12,16 @@ void hal_init(void)
 
 int hal_write(int fd, const void *buf, int nbytes)
 {
-	char* t = (char*)buf;
-	char tbuf[2];
-	if(nbytes==1){
-		tbuf[0]=*t;
-		tbuf[1]='\0';
-		hal_write_string(tbuf);
-		return nbytes;
-	}
-	hal_write_string(t);
-	return nbytes;
+  char* t = (char*)buf;
+  char tbuf[2];
+  if(nbytes==1){
+    tbuf[0]=*t;
+    tbuf[1]='\0';
+    hal_write_string(tbuf);
+    return nbytes;
+  }
+  if(nbytes<82)t[nbytes]='\0';//TODO: double check
+  hal_write_string(t);
+  return nbytes;
 }
 
